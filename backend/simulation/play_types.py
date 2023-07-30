0eb60cd32a32e788c2c7c1d91e05415eddd85d60
Tyler England <tyler_england35@yahoo.com>
2023-07-29 21:56:42 -0700

    Play type sims updated

--- backend/simulation/play_types.py
+++ backend/simulation/play_types.py
@@ -55,7 +55,7 @@ class Kick(Play):
 
     def sim(self):
         return_yds = random.randint(0, 30)
-        self.yds_gained = 25 if self.touchback else return_yds
+        self.yds_gained = 40 if self.touchback else self.yds_kicked - return_yds
         self.change_of_possession = True
 
 
@@ -64,11 +64,12 @@ class Punt(Play):
         super().__init__()
         self.play_type = "punt"
         self.yds_punted = random.randint(30, 60)
-        self.touchback = True if self.yds_punted > yds_to_goal else False
+        self.yds_to_goal = yds_to_goal
+        self.touchback = True if self.yds_punted > self.yds_to_goal else False
 
     def sim(self):
         return_yds = random.randint(0, 30)
-        self.yds_gained = 25 if self.touchback else return_yds
+        self.yds_gained = self.yds_to_goal - 25 if self.touchback else self.yds_punted - return_yds
         self.change_of_possession = True
 
 
