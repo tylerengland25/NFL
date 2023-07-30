9e09b380884ebabdac2e5bd838866dcd8b12f747
Tyler England <tyler_england35@yahoo.com>
2023-07-30 15:48:07 -0700

    Generalized play sim for pass and rush

--- backend/simulation/play_types.py
+++ backend/simulation/play_types.py
@@ -3,54 +3,55 @@ from abc import ABC, abstractmethod
 
 
 class Play(ABC):
-    def __init__(self) -> None:
+    def __init__(self, yds_to_goal, distance) -> None:
+        self.yds_to_goal = yds_to_goal
+        self.distance = distance
         self.play_type = None
         self.yds_gained = None
         self.duration = random.randint(5, 15)
         self.change_of_possession = False
+        self.touchdown = False
+        self.first_down = False
 
     @abstractmethod
     def sim(self):
-        pass
+        defensive_play = random.choice(["run", "pass"])
+        if defensive_play == self.play_type:
+            self.yds_gained = random.randint(-5, 5)
+        else:
+            self.yds_gained = random.randint(0, 15)
+        self.change_of_possession = random.choice(
+            [True] * 5 + [False] * 95
+        )
+        if self.yds_gained > self.yds_to_goal:
+            self.yds_gained = self.yds_to_goal
+            self.touchdown = True
+        elif self.yds_gained >= self.distance:
+            self.first_down = True
 
 
 class Pass(Play):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, yds_to_goal, distance) -> None:
+        super().__init__(yds_to_goal, distance)
         self.play_type = "pass"
 
     def sim(self):
-        defensive_play = random.choice(["run", "pass"])
-        if defensive_play == "pass":
-            self.yds_gained = random.randint(-5, 5)
-        else:
-            self.yds_gained = random.randint(0, 20)
-        self.change_of_possession = random.choice(
-            [True] * 5 + [False] * 95
-        )
+        super().sim()
 
 
 class Rush(Play):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, yds_to_goal, distance) -> None:
+        super().__init__(yds_to_goal, distance)
         self.play_type = "rush" 
 
     def sim(self):
-        defensive_play = random.choice(["run", "pass"])
-        if defensive_play == "run":
-            self.yds_gained = random.randint(-5, 5)
-        else:
-            self.yds_gained = random.randint(0, 10)
-        self.change_of_possession = random.choice(
-            [True] * 5 + [False] * 95
-        )
+        super().sim()
 
 
 class SpecialTeams(Play):
     def __init__(self, yds_to_goal) -> None:
-        super().__init__()
+        super().__init__(yds_to_goal, distance=None)
         self.yds_kicked = random.randint(30, 60)
-        self.yds_to_goal = yds_to_goal
         self.touchback = True if self.yds_kicked > self.yds_to_goal else False
 
     def sim(self):
@@ -58,11 +59,13 @@ class SpecialTeams(Play):
         self.yds_gained = self.yds_to_goal - 25 if self.touchback else self.yds_kicked - self.yds_returned
         self.change_of_possession = True
 
+
 class Kickoff(SpecialTeams):
     def __init__(self, yds_to_goal) -> None:
         super().__init__(yds_to_goal)
         self.play_type = "kickoff"
         self.yds_kicked = random.randint(40, 80)
+        self.touchback = True if self.yds_kicked > self.yds_to_goal else False
 
 
 class Punt(SpecialTeams):
@@ -73,7 +76,7 @@ class Punt(SpecialTeams):
 
 class FieldGoal(SpecialTeams):
     def __init__(self, yds_to_goal) -> None:
-        super().__init__()
+        super().__init__(yds_to_goal)
         self.play_type = "field goal"
         self.made = True if self.yds_kicked > yds_to_goal + 17 else False
 
