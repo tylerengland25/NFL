098892b763f0a61f83f976cf4bb43f4ec0e18f9b
Tyler England <tyler_england35@yahoo.com>
2023-07-29 20:44:56 -0700

    simulation folder

--- backend/simulation/simulation.py
+++ backend/simulation/simulation.py
@@ -1,4 +1,9 @@
 import random
+from play_types import (
+    Pass, Rush,
+    Kick, Punt,
+    FieldGoal
+)
 
 class Game:
     def __init__(self, home_team, away_team):
@@ -21,79 +26,41 @@ class Game:
         }
 
     def coin_toss(self):
-        return random.choice(["heads", "tails"])
-
+        coin_toss_result = random.choice(["heads", "tails"])
+        if coin_toss_result == "heads":
+            self.offense_team = self.home_team
+            self.defense_team = self.away_team
+            self.home_started = True
+        else:
+            self.offense_team = self.away_team
+            self.defense_team = self.home_team
+            self.home_started = False
+        self.yds_to_goal = 35
+        play_description = f"{self.defense_team} will receive the ball"
+        print(play_description)
+    
     def kickoff(self):
-        self.adjust_clock()
-        return random.randint(40, 64)
+        kickoff = Kick()
+        kickoff.play()
+        self.print_state(play=kickoff)
+        self.update_state(kickoff)
 
-    def play_offense(self):
-        self.play_type = random.choice(["pass", "rush"])
-        self.box_score[f"{self.offense_team}_{self.play_type}_plays"] += 1
-        if self.play_type == "pass":
-            self.secs_left -= random.randint(0, 20)
-            return random.randint(0, 30)
-        else:
-            self.adjust_clock()
-            return random.randint(0, 15)
-
-    def play_defense(self):
-        defense_play = random.choice(["pass", "rush"])
-        if defense_play == "pass":
-            return random.randint(-5, 5)
-        else:
-            return random.randint(-3, 3)
-
-    def punt(self):
-        self.adjust_clock()
-        punt_yards = random.randint(30, 60)
-        touchback = True if self.yds_to_goal - punt_yards < 0 else False
-        self.yds_to_goal = 75 if touchback else self.yds_to_goal - punt_yards
-        print(f"PUNT | {self.offense_team} punts the ball to {self.defense_team}")
+    def turnover_on_downs(self):
+        self.play_outcome = 'turnover on downs'
+        self.yds_to_goal -= self.yds_on_play
+        self.flip_field()
         self.change_possession()
-
-    def field_goal(self):
-        self.adjust_clock()
-        field_goal_yards = random.randint(30, 60)
-        if field_goal_yards > self.yds_to_goal + 17:
-            print(f"FIELD GOAL MISSED")
-            self.change_possession()
-            self.yds_to_goal = 100 - self.yds_to_goal
-        else:
-            self.adjust_score(self.score_field_goal())
-            print(f"FIELD GOAL MADE")
-            self.change_possession()
+        self.print_state()
 
     def change_possession(self):
         offense = self.offense_team
         self.offense_team = self.defense_team
         self.defense_team = offense
 
-    def score_touchdown(self):
-        return 7
+    def flip_field(self):
+        self.yds_to_goal = 100 - self.yds_to_goal
 
-    def score_field_goal(self):
         return 3
-
-    def print_score(self):
-        print(f"{self.home_team} {self.home_score} - {self.away_score} {self.away_team}\n")
-
-    def print_state(self):
-        possession = (
-            f"{self.away_team} @ {self.home_team}*" 
-            if self.offense_team == self.home_team 
-            else f"*{self.away_team} @ {self.home_team}"
-        )
-        quarter = f"Q{self.quarter}"
-        time_left = (
-            f"{self.secs_left // 60}:{self.secs_left % 60}" 
-            if self.secs_left % 60 >= 10 
-            else f"{self.secs_left // 60}:0{self.secs_left % 60}"
-        )
-        game_state = f"{possession} {quarter} {time_left}"
-        down_distance = f"{self.down} & {self.distance}"
-        yards_to_goal = f"{self.yds_to_goal} yards to goal"
-        print(f"{game_state} | {down_distance} | {yards_to_goal} | ", end="")
 
     def adjust_score(self, score):
         if self.offense_team == self.home_team:
@@ -107,21 +74,24 @@ class Game:
     def is_touchdown(self):
         if self.yds_to_goal < 0:
             self.box_score[f"{self.offense_team}_{self.play_type}_tds"] += 1
+            self.play_outcome = 'touchdown'
             self.adjust_score(self.score_touchdown())
-            print(f"TOUCHDOWN")
+            self.print_state()
             return True
         else:
             return False
 
     def is_turnover(self):
         if random.choice([True] + [False] * 19):
-            print(f"TURN OVER")
-            self.adjust_clock()
-            self.yds_to_goal = 100 - self.yds_to_goal
             if self.play_type == 'pass':
                 self.box_score[f"{self.offense_team}_ints"] += 1
+                self.play_outcome = 'interception'
             else:
                 self.box_score[f"{self.offense_team}_fums"] += 1
+                self.play_outcome = 'fumble'
+            self.print_state()
+            self.yds_to_goal -= self.yds_on_play
+            self.flip_field()
             self.change_possession()
             return True
         else:
@@ -129,7 +99,6 @@ class Game:
 
     def is_end_of_quarter(self):
         if self.secs_left <= 0:
-            print(f"END OF Q{self.quarter} | ", end="")
             self.print_score()
             self.secs_left = 900
             self.quarter += 1
@@ -137,12 +106,69 @@ class Game:
         else:
             return False
 
+    def print_score(self):
+        print(f"{self.home_team} {self.home_score} - {self.away_score} {self.away_team}\n")
+
+    def print_state(self, play):
+        # Set the possession string
+        possession = (
+            f"{self.away_team} @ {self.home_team}*" 
+            if self.offense_team == self.home_team 
+            else f"*{self.away_team} @ {self.home_team}"
+        )
+
+        # Set the clock string
+        quarter = f"Q{self.quarter}"
+        time_left = (
+            f"{self.secs_left // 60}:{self.secs_left % 60}" 
+            if self.secs_left % 60 >= 10 
+            else f"{self.secs_left // 60}:0{self.secs_left % 60}"
+        )
+        clock = f"{quarter} {time_left}"
+        
+        # Set the down and distance string
+        down_distance = f"{self.down} & {self.distance}" if play.play_type != 'kickoff' else 'Kickoff'
+        
+        # Set the yards to goal string
+        yards_to_goal = f"{self.yds_to_goal} yards to goal"
+        
+        # Set the play description string
+        if play.play_type == 'kickoff':
+            play_description = f"{self.offense_team} kicked the ball {play.yds_kicked} yards."
+            play_description = (
+                play_description + " Touchback." 
+                if play.touchback 
+                else play_description + f" Returned for {play.yds_gained} yards."
+            )
+
+        # Set the fixed width for each part of the printed output
+        possession = possession.ljust(10)
+        clock = clock.ljust(10)
+        down_distance = down_distance.ljust(10)
+        yards_to_goal = yards_to_goal.ljust(15)
+        play_description = play_description.ljust(40)
+        
+        print(f"| {possession} | {clock} | {down_distance} | {yards_to_goal} | {play_description}")
+
+
+    def update_state(self, play):
+        self.secs_left -= play.duration
+        self.yds_to_goal -= play.yds_gained
+        if play.change_of_possession:
+            self.change_possession()
+            self.flip_field()
+
+    def simulate_play(self):
+        play = random.choice([Pass(), Rush()])
+        play.play()
+        self.print_state(play=play)
+        self.update_state(play)
+
     def simulate_possession(self):
         # Initialize down and distance 
         self.down = 1
         self.distance = 10
-        print(f"{self.offense_team}'s ball. {self.down} & {self.distance} with {self.yds_to_goal} yards to goal")
-        
+    
         # Loop until punt, field goal or turnover
         while self.down <= 4:
             # Check for end of quarter
@@ -152,37 +178,34 @@ class Game:
                 if self.quarter == 5:
                     self.is_game_over = True
                     return "game over"
-
-            # Print game state
-            self.print_state()
             
             # Simlaute play
-            offense_yards = self.play_offense()
-            defense_bonus = self.play_defense()
-            total_yards = offense_yards + defense_bonus
+            self.simulate_play()
 
             # Account for a turnover
             if self.is_turnover():
                 return 'turnover'
 
-            # Adjust yards to goal
-            self.yds_to_goal -= total_yards
-
             # Adjust boxscore
-            self.box_score[f"{self.offense_team}_{self.play_type}_yards"] += total_yards
+            self.box_score[f"{self.offense_team}_{self.play_type}_yards"] += self.yds_on_play
             
             # Check for touchdown
             if self.is_touchdown():
                 return 'touchdown'
 
+            # Adjust yards to goal
+            self.yds_to_goal -= self.yds_on_play
+
             # Check for first down
-            if total_yards >= self.distance:
-                print(f"FRIST DOWN | {self.offense_team} gains {total_yards} yards.")
+            if self.yds_on_play >= self.distance:
+                self.play_outcome = 'first down'
+                self.print_state()
                 self.distance = 10 if self.yds_to_goal >= 10 else self.yds_to_goal
                 self.down = 1
             else:
-                print(f"{self.offense_team} gains {total_yards} yards.")
-                self.distance -= total_yards
+                self.play_outcome = ''
+                self.print_state()
+                self.distance -= self.yds_on_play
                 self.down += 1
 
             # Check for 4th down
@@ -194,40 +217,29 @@ class Game:
                     self.field_goal()
                     return 'field goal'
             elif self.down == 5:
-                self.change_possession()
-                self.yds_to_goal = 100 - self.yds_to_goal    
-                print("TUNROVER ON DOWNS")
+                self.turnover_on_downs()
                 return 'turnover'
 
     def simulate_game(self):
         # Kickoff to start the game
-        self.yds_to_goal = self.kickoff() + 35
+        self.kickoff()
 
         # Simulate possessions until end of quarter
         while not self.is_game_over:
             ended_in = self.simulate_possession()
             if ended_in == "touchdown" or ended_in == "field goal":
-                self.yds_to_goal = self.kickoff() + 35
+                self.kickoff()
             elif ended_in == "halftime":
                 # Kickoff to start the second half
                 self.offense_team = self.away_team if self.home_started else self.home_team
                 self.defense_team = self.home_team if self.home_started else self.away_team
-                self.yds_to_goal = self.kickoff() + 35
+                self.kickoff()
 
     def start(self):
         print(f"Welcome to the NFL Football Game Simulation between {self.home_team} and {self.away_team}!")
         
         # Perform the coin toss
-        coin_toss_result = self.coin_toss()
-        print(f"Coin toss: {coin_toss_result}")
-        if coin_toss_result == "heads":
-            self.offense_team = self.home_team
-            self.defense_team = self.away_team
-            self.home_started = True
-        else:
-            self.offense_team = self.away_team
-            self.defense_team = self.home_team
-            self.home_started = False
+        self.coin_toss()
 
         # Simulate the game
         self.simulate_game()
