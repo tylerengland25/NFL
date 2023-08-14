612961747b76156316c947d9f0eb04140915c415
Tyler England <tyler_england35@yahoo.com>
2023-08-13 20:21:53 -0700

    Backtest prints

--- backend/test/backtest.py
+++ backend/test/backtest.py
@@ -2,11 +2,10 @@ import sys
 sys.path.append('/home/tylerengland/NFL/')
 sys.path.append('/home/tylerengland/NFL/backend/simulation/')
 
-import numpy as np
-import nfl_data_py as nfl
+import pandas as pd
 from backend.simulation.simulation import Game
 from statistics import mean
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 
 class BackTest():
@@ -15,86 +14,12 @@ class BackTest():
         self.boxscores = {}
         self.predictions = {}
 
+        # Print settings
+        self.print_counter = 0
+
     def load_data(self):
-        backtest_years = [2022]
-        pbp = nfl.import_pbp_data(backtest_years, downcast=True, cache=False, alt_path=None)
-
-        # Set pandas display options
-        df = pbp.loc[~pbp['posteam_type'].isna(), :].copy()
-        df.fillna(0, inplace=True)
-
-        # Score columns
-        df.loc[:, 'home_score'] = df.loc[:, 'total_home_score'].astype(int)
-        df.loc[:, 'away_score'] = df.loc[:, 'total_away_score'].astype(int)
-
-        # Passing columns
-        df.loc[:, 'posteam_type'] = df.loc[:, 'posteam_type'].map({'home': 1, 'away': 0}).astype(int)
-        df.loc[:, 'home_pass_yards'] = np.where((df['pass_attempt'].astype(int)) & (df['posteam_type']), df['yards_gained'], 0).copy()
-        df.loc[:, 'away_pass_yards'] = np.where((df['pass_attempt'].astype(int)) & (~df['posteam_type']), df['yards_gained'], 0)
-        df.loc[:, 'home_pass_atts'] = np.where((df['pass_attempt'].astype(int)) & (df['posteam_type']), 1, 0)
-        df.loc[:, 'away_pass_atts'] = np.where((df['pass_attempt'].astype(int)) & (~df['posteam_type']), 1, 0)
-        df.loc[:, 'home_pass_tds'] = np.where((df['pass_touchdown'].astype(int)) & (df['posteam_type']), 1, 0)
-        df.loc[:, 'away_pass_tds'] = np.where((df['pass_touchdown'].astype(int)) & (~df['posteam_type']), 1, 0)
-        df.loc[:, 'home_pass_ints'] = np.where((df['interception'].astype(int)) & (df['posteam_type']), 1, 0)
-        df.loc[:, 'away_pass_ints'] = np.where((df['interception'].astype(int)) & (~df['posteam_type']), 1, 0)
-
-        # Rushing columns
-        df.loc[:, 'home_rush_yards'] = np.where((df['rush_attempt'].astype(int)) & (df['posteam_type']), df['yards_gained'], 0)
-        df.loc[:, 'away_rush_yards'] = np.where((df['rush_attempt'].astype(int)) & (~df['posteam_type']), df['yards_gained'], 0)
-        df.loc[:, 'home_rush_atts'] = np.where((df['rush_attempt'].astype(int)) & (df['posteam_type']), 1, 0)
-        df.loc[:, 'away_rush_atts'] = np.where((df['rush_attempt'].astype(int)) & (~df['posteam_type']), 1, 0)
-        df.loc[:, 'home_rush_tds'] = np.where((df['rush_touchdown'].astype(int)) & (df['posteam_type']), 1, 0)
-        df.loc[:, 'away_rush_tds'] = np.where((df['rush_touchdown'].astype(int)) & (~df['posteam_type']), 1, 0)
-        df.loc[:, 'home_rush_fums'] = np.where((df['fumble'].astype(int)) & (df['posteam_type']), 1, 0)
-        df.loc[:, 'away_rush_fums'] = np.where((df['fumble'].astype(int)) & (~df['posteam_type']), 1, 0)
-
-        # Defensive columns
-        df.loc[:, 'home_def_ints'] = np.where((df['interception'].astype(int)) & (~df['posteam_type']), 1, 0)
-        df.loc[:, 'away_def_ints'] = np.where((df['interception'].astype(int)) & (df['posteam_type']), 1, 0)
-        df.loc[:, 'home_def_fums'] = np.where((df['fumble'].astype(int)) & (~df['posteam_type']), 1, 0)
-        df.loc[:, 'away_def_fums'] = np.where((df['fumble'].astype(int)) & (df['posteam_type']), 1, 0)
-
-        # Special teams columns
-        df.loc[:, 'home_punt_atts'] = np.where((df['punt_attempt'].astype(int)) & (df['posteam_type']), 1, 0)
-        df.loc[:, 'away_punt_atts'] = np.where((df['punt_attempt'].astype(int)) & (~df['posteam_type']), 1, 0)
-        df.loc[:, 'home_fg_atts'] = np.where((df['field_goal_attempt'].astype(int)) & (df['posteam_type']), 1, 0)
-        df.loc[:, 'away_fg_atts'] = np.where((df['field_goal_attempt'].astype(int)) & (~df['posteam_type']), 1, 0)
-        df.loc[:, 'home_fg_made'] = np.where((df['field_goal_result'] == 'made') & (df['posteam_type']), 1, 0)
-        df.loc[:, 'away_fg_made'] = np.where((df['field_goal_result'] == 'made') & (~df['posteam_type']), 1, 0)
-
-        game_cols = [
-            'game_id', 'game_date', 'home_team', 'away_team', 'season_type', 'week', 'season',
-        ]
-
-        # Aggregate stats
-        df = (
-            df.groupby(game_cols)
-            .agg(
-                {
-                    'home_score': 'max', 'away_score': 'max',
-                    'home_pass_yards': 'sum', 'away_pass_yards': 'sum',
-                    'home_pass_atts': 'sum', 'away_pass_atts': 'sum',
-                    'home_pass_tds': 'sum', 'away_pass_tds': 'sum',
-                    'home_pass_ints': 'sum', 'away_pass_ints': 'sum',
-                    'home_rush_yards': 'sum', 'away_rush_yards': 'sum',
-                    'home_rush_atts': 'sum', 'away_rush_atts': 'sum',
-                    'home_rush_tds': 'sum', 'away_rush_tds': 'sum',
-                    'home_rush_fums': 'sum', 'away_rush_fums': 'sum',
-                    'home_def_ints': 'sum', 'away_def_ints': 'sum',
-                    'home_def_fums': 'sum', 'away_def_fums': 'sum',
-                    'home_punt_atts': 'sum', 'away_punt_atts': 'sum',
-                    'home_fg_atts': 'sum', 'away_fg_atts': 'sum',
-                    'home_fg_made': 'sum', 'away_fg_made': 'sum'
-                }
-            )
-            .reset_index()
-        )
-
-        # Calculate spread and total
-        df['spread'] = df['home_score'] - df['away_score']
-        df['total'] = df['home_score'] + df['away_score']
-
-        return df
+        df = pd.read_csv('/home/tylerengland/NFL/backend/test/data.csv')
+        self.data = df
 
     def update_boxscores(self, boxscore):
         for stat in boxscore:
@@ -113,37 +38,53 @@ class BackTest():
 
     def evaluate(self):
         for stat in self.predictions:
-            actual = self.actuals[stat]
-            pred = self.predictions[stat]
-            mse = mean_squared_error(actual, pred)
-            r2 = r2_score(actual_scores, sim_scores)
+            actuals = self.data[stat]
+            preds = self.predictions[stat]
+            rmse = mean_squared_error(actuals, preds, squared=False)
+            mae = mean_absolute_error(actuals, preds)
             
-            print(f"Team: {team}")
-            print(f"Actual Scores: {actual_scores}")
-            print(f"Simulated Scores: {sim_scores}")
-            print(f"MSE: {mse}, R2: {r2}")
-            print("====================")
+            # Headers and states
+            headers = ["Stat", "RMSE", "MAE"]
+            data = [stat, str(round(rmse, 2)), str(round(mae, 2))]
+
+            # Set column widths
+            preset_widths = [20, 7, 7]
+            column_widths = [max(len(header), width) for header, width in zip(headers, preset_widths)]
+
+            # Header row
+            header_row = " | ".join(header.ljust(width) for header, width in zip(headers, column_widths))
+            data_row = " | ".join(state.ljust(width) for state, width in zip(data, column_widths))
+
+            # Print the header ever 15 plays
+            if self.print_counter % 15 == 0:
+                print("-" * len(header_row))
+                print(header_row)
+                print("-" * len(header_row))
+            print(data_row)
+            self.print_counter += 1
 
     def run(self):
-        games = self.load_data()
+        self.load_data()
 
-        for index, game in games.iterrows():
+        for index, game in self.data.iterrows():
+            print(game['game_id'])
             # Extract game data
             home_team = game['home_team']
             away_team = game['away_team']
 
             # Simulate game and update boxscores
-            game_sim = Game(home_team, away_team, verbose=False)
             for _ in range(self.num_sims):
+                game_sim = Game(home_team, away_team, verbose=False)
                 boxscore = game_sim.simulate_game()
+                boxscore['spread'] = boxscore['home_score'] - boxscore['away_score']
+                boxscore['total'] = boxscore['home_score'] + boxscore['away_score']
                 self.update_boxscores(boxscore)
 
             # Aggregate boxscores
             self.aggregate_boxscores()
-        
-        
+                
         self.evaluate()
 
 if __name__ == '__main__':
-    backtest = BackTest(num_sims=5000)
+    backtest = BackTest(num_sims=1)
     backtest.run()
